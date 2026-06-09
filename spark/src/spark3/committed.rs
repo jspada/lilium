dd568cab4ec0c8abdcddfbf127cded9432af6b7e
fabri236 <fabrizio_m@hotmail.com>
2026-06-09 19:03:17 -0300

    implement verifier

--- spark/src/spark3/committed.rs
+++ spark/src/spark3/committed.rs
@@ -2,12 +2,16 @@ use crate::spark3::{
     sumcheck_argument::SparkEvals, SparkInstance, SparseMle, StaticSparkRelation,
     StaticSparkStructure, BYTE,
 };
-use ark_ff::Field;
+use ark_ff::{batch_inversion, Field};
 use commit::commit2::{oracle::CommittedOracle, CommitmentScheme};
 use std::{marker::PhantomData, rc::Rc};
-use sumcheck::sumcheck2::{
-    evals::Evals,
-    oracles::{composite::CompositeOracle, core::CoreOracle, SumcheckFunction},
+use sumcheck::{
+    eq,
+    polynomials::MultiPoint,
+    sumcheck2::{
+        evals::Evals,
+        oracles::{composite::CompositeOracle, core::CoreOracle, SumcheckFunction},
+    },
 };
 use transcript::reduction2::Relation;
 
@@ -32,13 +36,47 @@ impl<const N: usize> MinorStructure<N> {
 
         Self { counts }
     }
+
+    /// Computes the sums at the left of the equation, thanks to restricting the
+    /// lookup table to 8 bits it can be done in about 256 operations.
+    pub(crate) fn expected_sums<F: Field>(
+        &self,
+        point: &[MultiPoint<F>; N],
+        lookup_challenge: F,
+        compression_challenge: F,
+    ) -> [F; N] {
+        let mut res = [F::zero(); N];
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..N {
+            let point = &point[i];
+            let counts = &self.counts[i];
+            let mut denominators = eq::eq(point);
+            for (i, e) in denominators.iter_mut().enumerate() {
+                let address = F::from(i as u8);
+                *e = address * compression_challenge + *e + lookup_challenge;
+            }
+            batch_inversion(&mut denominators);
+            let inverses = denominators;
+            res[i] = inverses
+                .into_iter()
+                .zip(counts.iter())
+                .fold(F::zero(), |acc, e| {
+                    let (inverse, count) = e;
+                    let count = F::from(*count as u64);
+                    acc + inverse * count
+                });
+        }
+        res
+    }
 }
 
-type Oracle<F, C, SF> = CompositeOracle<F, SF, CoreOracle<F, SF>, CommittedOracle<F, C, SF>>;
+pub type Oracle<F, C, SF> = CompositeOracle<F, SF, CoreOracle<F, SF>, CommittedOracle<F, C, SF>>;
+pub type SparkOracle<F, C, const N: usize> = Oracle<F, C, SparkEvals<(), N>>;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct CommittedSparkStructure<F: Field, C: CommitmentScheme<F>, const N: usize> {
-    oracle: Oracle<F, C, SparkEvals<(), N>>,
+    oracle: SparkOracle<F, C, N>,
     minor_structure: MinorStructure<N>,
     mle: Rc<SparseMle<F, N>>,
     pcs: C,
@@ -76,7 +114,7 @@ where
     }
 }
 
-fn oracle<F, C, const N: usize>(mles: &SparseMle<F, N>, pcs: C) -> Oracle<F, C, SparkEvals<(), N>>
+fn oracle<F, C, const N: usize>(mles: &SparseMle<F, N>, pcs: C) -> SparkOracle<F, C, N>
 where
     F: Field,
     C: CommitmentScheme<F>,
--- spark/src/spark3/reduction.rs
+++ spark/src/spark3/reduction.rs
@@ -1,8 +1,27 @@
-use crate::spark3::{SparkInstance, StaticSparkRelation, StaticSparkStructure};
+use crate::spark3::{
+    committed::{MinorStructure, SparkOracle},
+    sumcheck_argument::SparkEvals,
+    SparkInstance, StaticSparkRelation, StaticSparkStructure,
+};
 use ark_ff::Field;
-use commit::commit2::{CommitmentScheme, OpenInstance, OpeningRelation};
+use commit::commit2::{
+    oracle::{CommittedOracle, CommittedOracleInstance},
+    CommitmentScheme, OpenInstance, OpeningRelation,
+};
 use sponge::sponge::Duplex;
 use std::marker::PhantomData;
+use sumcheck::{
+    polynomials::MultiPoint,
+    sumcheck2::{
+        oracles::{
+            composite::{CompositeOracle, CompositeReductionKey, ProverEvals},
+            core::{CoreOracle, CoreOracleInstance},
+            partial::{Nature, PartialOracle, PartialQueryInstance},
+            SumcheckFunction,
+        },
+        SumcheckInstance, SumcheckMessage, SumcheckReduction, SumcheckVerifierKey,
+    },
+};
 use transcript::reduction2::{
     GuardedProof, ProverOutput, Reduction, Transcript, TranscriptBuilder, VerifierTranscript,
 };
@@ -12,6 +31,40 @@ pub struct SparkReduction<F: Field, C: CommitmentScheme<F>, const N: usize>(Phan
 type Rel1<F, const N: usize> = StaticSparkRelation<F, N>;
 type Rel2<F, C> = OpeningRelation<F, C>;
 
+#[derive(Clone, Debug)]
+pub struct Proof<F: Field, C: CommitmentScheme<F>, const N: usize> {
+    lookup_commitments: [C::Commitment; N],
+    inverse_commitments: [C::Commitment; N],
+    sumcheck_proof: Vec<SumcheckMessage<F>>,
+    oracle_query_proof: ProverEvals<F>,
+}
+
+fn split_point<F: Field, const N: usize>(point: &MultiPoint<F>) -> [MultiPoint<F>; N] {
+    point
+        .inner_ref()
+        .chunks(8)
+        .map(|segment| {
+            assert_eq!(segment.len(), 8);
+            MultiPoint::new(segment.to_vec())
+        })
+        .collect::<Vec<MultiPoint<F>>>()
+        .try_into()
+        .unwrap()
+}
+
+pub struct Key<F, C, SF, const N: usize>
+where
+    F: Field,
+    C: CommitmentScheme<F>,
+    SF: SumcheckFunction<F>,
+    SF::Natures: Nature,
+    CommittedOracle<F, C, SF>: PartialOracle<F, SF>,
+{
+    minor_structure: MinorStructure<N>,
+    sumcheck_key: SumcheckVerifierKey<F>,
+    oracle_key: CompositeReductionKey<F, SF, CoreOracle<F, SF>, CommittedOracle<F, C, SF>>,
+}
+
 impl<F, C, const N: usize> Reduction<F, Rel1<F, N>, Rel2<F, C>> for SparkReduction<F, C, N>
 where
     F: Field,
@@ -19,9 +72,9 @@ where
 {
     type ProverKey = ();
 
-    type VerifierKey = ();
+    type VerifierKey = Key<F, C, SparkEvals<(), N>, N>;
 
-    type Proof = ();
+    type Proof = Proof<F, C, N>;
 
     type Error = ();
 
@@ -56,11 +109,90 @@ where
     }
 
     fn verify<S: Duplex<F>>(
-        _key: &Self::VerifierKey,
-        _instance: SparkInstance<F>,
-        _proof: GuardedProof<Self::Proof>,
-        _transcript: &mut VerifierTranscript<F, S>,
+        key: &Self::VerifierKey,
+        instance: SparkInstance<F>,
+        proof: GuardedProof<Self::Proof>,
+        transcript: &mut VerifierTranscript<F, S>,
     ) -> Result<OpenInstance<F, C>, Self::Error> {
-        todo!()
+        // FIRST ROUND: Sending commitments to lookups, getting challenges
+        // for lookup argument.
+        let Ok((lookup_commitments, [c1, c2])) =
+            transcript.receive_message(|proof| proof.lookup_commitments.clone(), &proof, &());
+
+        let lookup_challenge = c1;
+        let compression_challenge = c2;
+
+        let zerocheck_point = MultiPoint::new(transcript.point());
+
+        // SECOND ROUND: Sending commitments to the inverses of the indexed lookups.
+        // Another challenge is received to combine the multiple sumchecks into 1.
+        let Ok((inverse_commitments, [c3])) =
+            transcript.receive_message(|proof| proof.inverse_commitments.clone(), &proof, &());
+        let combination_challenge = c3;
+
+        let SparkInstance { point, eval } = instance;
+
+        let points = split_point(&point);
+        // Expected sums from computing the left side directly.
+        let expected_sums =
+            key.minor_structure
+                .expected_sums(&points, lookup_challenge, compression_challenge);
+
+        let sum = expected_sums
+            .into_iter()
+            .fold(F::zero(), |acc, s| acc * combination_challenge + s);
+        let sum = sum * combination_challenge + eval;
+
+        let sumcheck_instance = sumcheck_instance::<F, C, N>(
+            sum,
+            lookup_commitments,
+            inverse_commitments,
+            zerocheck_point,
+        );
+
+        let sumcheck_proof = proof.clone().map(|proof| proof.sumcheck_proof);
+        //TODO: handle
+        let query_instance = SumcheckReduction::verify(
+            &key.sumcheck_key,
+            sumcheck_instance,
+            sumcheck_proof,
+            transcript,
+        )
+        .unwrap();
+
+        let oracle_proof = proof.map(|proof| proof.oracle_query_proof);
+        //TODO: handle
+        let red =
+            CompositeOracle::verify(&key.oracle_key, query_instance, oracle_proof, transcript)
+                .unwrap();
+
+        let (core_query, committed_query) = red;
+        let core_query: PartialQueryInstance<F, CoreOracleInstance<F, _>> = core_query;
+
+        let core_proof = GuardedProof::empty();
+        //TODO: handle
+        CoreOracle::verify(key.oracle_key.p1_key(), core_query, core_proof, transcript).unwrap();
+
+        let committed_query: PartialQueryInstance<F, CommittedOracleInstance<F, C, _>> =
+            committed_query;
+
+        let proof = GuardedProof::empty();
+        let Ok(open_instance) =
+            CommittedOracle::verify(key.oracle_key.p2_key(), committed_query, proof, transcript);
+
+        Ok(open_instance)
     }
+}
+
+fn sumcheck_instance<F, C, const N: usize>(
+    _sum: F,
+    _lookup_commitments: [C::Commitment; N],
+    _inverse_commitments: [C::Commitment; N],
+    _zerocheck_point: MultiPoint<F>,
+) -> SumcheckInstance<F, SparkOracle<F, C, N>>
+where
+    F: Field,
+    C: CommitmentScheme<F>,
+{
+    todo!()
 }
